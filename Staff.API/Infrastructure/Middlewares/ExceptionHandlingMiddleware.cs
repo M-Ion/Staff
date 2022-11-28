3989d4d23a80667bcd7bd1182ee62da72398abc7
M-Ion <mereutan05@gmail.com>
2022-11-28 17:03:17 +0200

    Add global error handler

---
+++ Staff.API/Infrastructure/Middlewares/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using Staff.Common.Models;
+using System.Net;
+
+namespace Staff.API.Infrastructure.Middlewares
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            context.Response.ContentType = "application/json";
+
+            var errDetails = new ErrorDetails()
+            {
+                StatusCode = (int)HttpStatusCode.InternalServerError,
+                Status = "Failure",
+                Message = ex.Message
+            };
+
+            switch (ex)
+            {
+                default:
+                    break;
+            }
+
+            context.Response.StatusCode = errDetails.StatusCode;
+
+            return context.Response.WriteAsync(errDetails.ToString());
+        }
+    }
+}
